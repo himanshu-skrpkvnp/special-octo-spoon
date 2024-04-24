4d7c9bb68f56bc236e0a4d52ece6f13f838c2ef1
himanshu-skrpkvnp <himanshusinghskrpkvnp@gmail.com>
2024-04-24 12:02:55 +0530

    added_api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor ( private http : HttpClient) {}
   title = 'angular_second';
+  ngOnInit() {
+		// API Call
+		let headers = new HttpHeaders({
+			'x-rapidapi-host': 'covid-193.p.rapidapi.com',
+			'x-rapidapi-key': '710aad5e87msh11210aa01027da0p1f86dbjsn7b87e799b40c'
+		});
+    
+		this.http
+			.get<any>('https://covid-193.p.rapidapi.com/countries', {
+				headers: headers
+			})
+			.subscribe(data => {
+				console.log(data);
+			});
+	}
 }
