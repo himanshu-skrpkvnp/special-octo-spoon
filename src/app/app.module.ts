976fc59fadf2a33cd78418a32183fbe21f9435a0
himanshu-skrpkvnp <himanshusinghskrpkvnp@gmail.com>
2024-04-24 15:56:17 +0530

    home component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http' ;
+import { HttpClientModule } from '@angular/common/http';
+// import { AbcdComponent } from './abcd/abcd.component' ;
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent  ,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
